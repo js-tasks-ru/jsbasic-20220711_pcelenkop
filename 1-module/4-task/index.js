e191b089f9c3acef0e178484d05de58db673ddde
pcelenkop <pcelenkop@gmail.com>
2022-07-24 22:41:50 +0300

    Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  const newStr = str.toLowerCase();
+    const spam = "1xbet";
+    const spam1 = "xxx";
+    if (newStr.includes(spam) || (newStr.includes(spam1))) { 
+      return true;
+  }   
+  else {
+      return false;
+  }
 }
